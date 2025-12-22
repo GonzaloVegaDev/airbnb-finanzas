018bb24788335835ce5a55276a2710ee2c49a0cb
Gonzalo Alvarez Vega <alvarezvegagonzalo@gmail.com>
2025-12-22 09:36:20 -0600

    commit de dia 2

--- config/settings.py
+++ config/settings.py
@@ -79,10 +79,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
-}
 
 
 # Password validation
