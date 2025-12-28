a15020ada7d17820a16504dc058a8473a859fe6c
Gonzalo Alvarez Vega <alvarezvegagonzalo@gmail.com>
2025-12-27 22:37:42 -0600

    beta 0.1 para render

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY
 SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-secret-key")
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 # Applications
